467485a88cb66fe87aee46a3fe349700338da1bb
duqingxiang <duqingxiang@huobi.com>
2019-09-25 19:59:52 +0800

    add candleck id field

--- src/test/java/com/huobi/client/examples/SubscribeCandlestickEvent.java
+++ src/test/java/com/huobi/client/examples/SubscribeCandlestickEvent.java
@@ -10,6 +10,7 @@ public class SubscribeCandlestickEvent {
     String symbol = "btcusdt";
     subscriptionClient.subscribeCandlestickEvent(symbol, CandlestickInterval.MIN15, (candlestickEvent) -> {
       System.out.println("--------------- Subscribe Candlestick ------------------");
+      System.out.println("id: " + candlestickEvent.getData().getId());
       System.out.println("Timestamp: " + candlestickEvent.getData().getTimestamp());
       System.out.println("High: " + candlestickEvent.getData().getHigh());
       System.out.println("Low: " + candlestickEvent.getData().getLow());
@@ -21,12 +22,14 @@ public class SubscribeCandlestickEvent {
     subscriptionClient.requestCandlestickEvent(symbol,null,null,CandlestickInterval.MIN15, (candlestickEvent) -> {
       System.out.println("--------------- Request Candlestick ------------------");
       candlestickEvent.getData().forEach(candlestick -> {
+        System.out.println("id: " + candlestick.getId());
         System.out.println("Timestamp: " + candlestick.getTimestamp());
         System.out.println("High: " + candlestick.getHigh());
         System.out.println("Low: " + candlestick.getLow());
         System.out.println("Open: " + candlestick.getOpen());
         System.out.println("Close: " + candlestick.getClose());
         System.out.println("Volume: " + candlestick.getVolume());
+        System.out.println("--------------------------------------------");
       });
     });
   }
