bb092a2cfc0c0e8fb8b8bc1f4b694d4029209931
yuanxueqi <yuanxueqi@huobi.com>
2020-04-02 15:59:25 +0800

    1.0.11

--- src/test/java/com/huobi/client/examples/constants/Constants.java
+++ src/test/java/com/huobi/client/examples/constants/Constants.java
@@ -2,7 +2,7 @@ package com.huobi.client.examples.constants;
 
 public class Constants {
 
-  public static final String API_KEY = "cdgs9k03f3-9d0f3476-765877ed-c6bea";
-  public static final String SECRET_KEY = "7cf28044-368889c0-829e653e-d7bcc";
+  public static final String API_KEY = "xxxx";
+  public static final String SECRET_KEY = "oooo";
 
 }
