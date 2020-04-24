66de8802d3ce9bdd4acbf4191aeb6622e0e5356d
kruchinin-pavel <kruchinin.1977@ya.ru>
2020-04-24 11:17:39 +0300

    fixing issues

--- src/test/java/com/huobi/client/examples/constants/Constants.java
+++ src/test/java/com/huobi/client/examples/constants/Constants.java
@@ -2,7 +2,7 @@ package com.huobi.client.examples.constants;
 
 public class Constants {
 
-  public static final String API_KEY = "";
-  public static final String SECRET_KEY = "";
+  public static final String API_KEY = "xxxx";
+  public static final String SECRET_KEY = "oooo";
 
 }
