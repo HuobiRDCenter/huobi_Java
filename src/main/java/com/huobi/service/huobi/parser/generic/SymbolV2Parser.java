d0f6600ef52774132f06e9264eb9b0fea6c3b133
mikhniuk <r.mikhniuk@gmail.com>
2024-03-31 00:29:47 +0400

    Fix SymbolV2Parser

--- src/main/java/com/huobi/service/huobi/parser/generic/SymbolV2Parser.java
+++ src/main/java/com/huobi/service/huobi/parser/generic/SymbolV2Parser.java
@@ -2,23 +2,61 @@ package com.huobi.service.huobi.parser.generic;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.huobi.model.generic.Person;
 import com.huobi.model.generic.Symbol;
 import com.huobi.model.generic.SymbolV2;
 import com.huobi.service.huobi.parser.HuobiModelParser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class SymbolV2Parser implements HuobiModelParser<SymbolV2> {
 
     @Override
     public SymbolV2 parse(JSONObject json) {
-        return json.toJavaObject(SymbolV2.class);
+        if (json.get("p1") == null)
+            return null;
+
+        SymbolV2 result = new SymbolV2();
+        result.setP1(new Person(((JSONObject)((JSONArray) json.get("p1")).getFirst()).getInteger("id"),
+                ((JSONObject)((JSONArray) json.get("p1")).getFirst()).getString("name"),
+                ((JSONObject)((JSONArray) json.get("p1")).getFirst()).getInteger("weight")));
+        result.setBc(json.getString("bc"));
+        result.setLr(json.getBigDecimal("lr"));
+        result.setToa(json.getLong("toa"));
+        result.setFp(json.getBigDecimal("fp"));
+        result.setDn(json.getString("dn"));
+        result.setSc(json.getString("sc"));
+        result.setQc(json.getString("qc"));
+        result.setState(json.getString("state"));
+        result.setWr(json.getString("wr"));
+        result.setSp(json.getString("sp"));
+        result.setCd(json.getBoolean("cd"));
+        result.setTap(json.getBigDecimal("tap"));
+        result.setBcdn(json.getString("bcdn"));
+        result.setQcdn(json.getString("qcdn"));
+        result.setTe(json.getBoolean("te"));
+        result.setTtp(json.getBigDecimal("ttp"));
+        result.setTpp(json.getBigDecimal("tpp"));
+        result.setWhe(json.getBoolean("whe"));
+        result.setW(json.getInteger("w"));
+
+        return result;
     }
 
     @Override
     public SymbolV2 parse(JSONArray json) {
+        System.out.println(json);
         return null;
+    }
+
+    public SymbolV2 parse(Object json) {
+        if (json instanceof JSONArray)
+            return parse((JSONArray) json);
+        else
+            return parse((JSONObject) json);
     }
 
     @Override
@@ -27,10 +65,9 @@ public class SymbolV2Parser implements HuobiModelParser<SymbolV2> {
             return new ArrayList<>();
         }
 
-        List<SymbolV2> list = new ArrayList<>();
-        for (int i = 0; i < jsonArray.size(); i++) {
-            list.add(parse(jsonArray.getJSONObject(i)));
-        }
-        return list;
+        return (List<SymbolV2>) jsonArray.stream()
+                .map(this::parse)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 }
